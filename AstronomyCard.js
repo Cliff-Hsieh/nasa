8b0462b254bec620e6d4e150a9861ede6b2d1969
Cliff <qm29213039@gmail.com>
2017-10-25 15:37:13 +0800

    Add Styles

--- AstronomyCard.js
+++ AstronomyCard.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
 
 const AstronomyCard = (props) => {
+  const { date, explanation, hdurl, title, url } = props.data;
 
-    const { date, explanation, hdurl, title, url } = props.data;
-
-    return (
-      <View>
-        <Text>{ title }</Text>
-        <Image src={ url } alt={ title } />
-        <Text>{ explanation }</Text>
-        <Text>{ date }</Text>
-      </View>
-    )
+  return (
+    <View style={ styles.container }>
+      <Text>{ title }</Text>
+      <Image src={ url } alt={ title } />
+      <Text style={ styles.welcome } >{ explanation }</Text>
+      <Text>{ date }</Text>
+    </View>
+  )
 }
 
 export default AstronomyCard;
