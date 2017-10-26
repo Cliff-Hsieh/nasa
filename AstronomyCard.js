c91bbdea238b2f197adc72e7dfb39701177adfd9
Cliff <qm29213039@gmail.com>
2017-10-26 18:09:46 +0800

    (feat): state toggle by react

--- AstronomyCard.js
+++ AstronomyCard.js
@@ -2,19 +2,34 @@ import React from 'react';
 import { View, Text, Image, ScrollView, TouchableHighlight, Linking } from 'react-native';
 import styles from './Styles';
 
-const AstronomyCard = (props) => {
-  const { date, explanation, hdurl, title, url } = props.data;
+class AstronomyCard extends React.Component {
+    state = {
+      show: true
+    };
 
-  return (
-      <ScrollView>
-      <Text style={ styles.title }>{ title }</Text>
-      <TouchableHighlight onPress={() => Linking.openURL( hdurl ) }>
-        <Image source={{ uri: url }} style={ styles.img } alt={ title }S  />
-      </TouchableHighlight>
-      <Text style={ styles.post } >{ explanation }</Text>
-      <Text style={ styles.copyright }>{ date }</Text>
-      </ScrollView>
-  )
+    onPress = () => {
+        const { show } = this.state;
+        this.setState({ show: !show });
+    };
+
+    render() {
+        const { date, explanation, hdurl, title, url } = this.props.data;
+        const { show } = this.state;
+        return (
+            <ScrollView>
+                <Text style={ styles.title }>{ title }</Text>
+                <TouchableHighlight onPress={this.onPress}>
+                    <Image source={{ uri: url }} style={ styles.img } alt={ title }S  />
+                </TouchableHighlight>
+                {
+                    show
+                    ? <Text style={ styles.post } >{ explanation }</Text>
+                    : null
+                }
+                <Text style={ styles.copyright }>{ date }</Text>
+            </ScrollView>
+        )
+    }
 }
 
 export default AstronomyCard;
